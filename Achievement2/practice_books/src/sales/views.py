fa5ca5bf54d5788635835f92de7be9a04f5cf13c
joannawongww <joanna_1994@hotmail.com>
2023-11-30 14:47:06 +0800

    register home view for Bookstore app

--- Achievement2/practice_books/src/sales/views.py
+++ Achievement2/practice_books/src/sales/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# define home function
+# function takes request from web app
+# returns template at sales/home.html as response
+
+
+def home(request):
+    return render(request, 'sales/home.html')
